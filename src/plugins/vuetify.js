32704dc88bcdc9e09b6918a4c7d41f0fb2d7e89e
Theo <adryantheo@gmail.com>
2020-08-02 09:03:57 +0700

    Added new configurations for vuetify v2

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,11 +1,32 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
 import VueClipboard from 'vue-clipboard2'
+import colors from 'vuetify/lib/util/colors';
 
 
 Vue.use(Vuetify);
 Vue.use(VueClipboard);
 
-const opts = {}
+// const opts = {}
 
-export default new Vuetify(opts)
+// export default new Vuetify(opts)
+export default new Vuetify({
+    icons: {
+        iconfont: 'mdi', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+      },
+      theme: {
+        themes: {
+          light: {
+            primary: colors.green.darken1, // #E53935
+            secondary: colors.red.lighten4, // #FFCDD2
+            accent: colors.indigo.base, // #3F51B5
+          },
+          dark: {
+            primary: colors.red.darken2, // #E53935
+            secondary: colors.red.lighten4, // #FFCDD2
+            accent: colors.indigo.base, // #3F51B5
+          },
+        },
+      },
+
+});
